533e9bdb852ca96c8cfe1943e5191eda81aaf405
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-19 12:34:21 +0900

    ⚙️ Chore: 로고 import index 파일 추가

---
+++ src/assets/logos/index.ts
@@ -0,0 +1,9 @@
+import Large from '@/assets/logos/logo-lg.svg';
+import Medium from '@/assets/logos/logo-m.svg';
+import Small from '@/assets/logos/logo-s.svg';
+
+export default {
+  Large,
+  Medium,
+  Small,
+};
