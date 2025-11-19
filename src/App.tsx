1715f4e6ad6662e2bfdf473143c2f5a28796da38
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-19 10:01:29 +0900

    [⚙️ Chore/11] 리액트 라우터 설정 (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Router from '@/router/Router';
+
 function App() {
-  return <>메인</>;
+  return <Router />;
 }
 
 export default App;
---
+++ src/router/Router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from '@/pages/Home';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
