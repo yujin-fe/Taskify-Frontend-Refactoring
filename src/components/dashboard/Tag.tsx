051226d622e0489851c94b0733c2d7063e75c915
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-19 22:44:27 +0900

    ✨ Feat: 태그 컴포넌트 구현

---
+++ src/components/dashboard/Tag.tsx
@@ -0,0 +1,27 @@
+import { cva } from 'class-variance-authority';
+import type { ColorType } from '@/types/ui';
+import { cn } from '@/utils/cn';
+
+const tagStyles = cva(
+  'inline-block px-1.5 py-1 rounded-sm font-xs-regular sm:py-0.5 sm:font-md-regular',
+  {
+    variants: {
+      color: {
+        orange: 'text-orange-500 bg-orange-500/8',
+        blue: 'text-blue-500 bg-blue-500/8',
+        green: 'text-green-500 bg-green-500/8',
+        pink: 'text-pink-500 bg-pink-500/8',
+        purple: 'text-purple-500 bg-purple-500/8',
+      },
+    },
+  }
+);
+
+interface TagProps {
+  children: React.ReactNode;
+  color: ColorType;
+}
+
+export default function Tag({ children, color }: TagProps) {
+  return <span className={cn(tagStyles({ color }))}>{children}</span>;
+}
