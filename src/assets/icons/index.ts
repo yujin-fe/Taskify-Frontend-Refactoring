d724a5f04937813d40374e089c9c3ec10c5188aa
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-19 21:34:24 +0900

    [⚙️ Chore/24] 아이콘 에셋 파일 및 index.ts 추가 (#31)

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,39 @@
+import PasswordHidden from '@/assets/icons/auth/ic-password-hidden.svg';
+import PasswordShow from '@/assets/icons/auth/ic-password-show.svg';
+import AddDashboard from '@/assets/icons/dashboard/ic-add-dashboard.svg';
+import ArrowDropdown from '@/assets/icons/dashboard/ic-arrow-dropdown.svg';
+import ArrowLeft from '@/assets/icons/dashboard/ic-arrow-left.svg';
+import ArrowRight from '@/assets/icons/dashboard/ic-arrow-right.svg';
+import Calendar from '@/assets/icons/dashboard/ic-calendar.svg';
+import Check from '@/assets/icons/dashboard/ic-check.svg';
+import Close from '@/assets/icons/dashboard/ic-close.svg';
+import Crown from '@/assets/icons/dashboard/ic-crown.svg';
+import KebabMore from '@/assets/icons/dashboard/ic-kebab-more.svg';
+import Pencil from '@/assets/icons/dashboard/ic-pencil.svg';
+import Plus from '@/assets/icons/dashboard/ic-plus.svg';
+import Search from '@/assets/icons/dashboard/ic-search.svg';
+import Setting from '@/assets/icons/dashboard/ic-setting.svg';
+import Email from '@/assets/icons/home/ic-email.svg';
+import Facebook from '@/assets/icons/home/ic-facebook.svg';
+import Instagram from '@/assets/icons/home/ic-instagram.svg';
+
+export default {
+  PasswordHidden,
+  PasswordShow,
+  AddDashboard,
+  ArrowDropdown,
+  ArrowLeft,
+  ArrowRight,
+  Calendar,
+  Check,
+  Close,
+  Crown,
+  KebabMore,
+  Pencil,
+  Plus,
+  Search,
+  Setting,
+  Email,
+  Facebook,
+  Instagram,
+};
