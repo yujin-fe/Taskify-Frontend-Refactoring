08158d3905a4af6192afa41eacd2350f8814fa61
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-19 11:50:29 +0900

    ⚙️ Chore: vite 설정에 svgr 플러그인 추가 및 타입 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { fileURLToPath } from 'url';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr({ include: '**/*.svg' })],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
