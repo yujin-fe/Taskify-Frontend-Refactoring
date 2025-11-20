f10c098bf1e5d3194ba62028743238878bede8ac
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-20 23:08:22 +0900

    ⚙️ Chore: type 정의 방식 변경

--- src/components/dashboard/dropdown/DropdownMenuItem.tsx
+++ src/components/dashboard/dropdown/DropdownMenuItem.tsx
@@ -1,8 +1,4 @@
-interface DropdownItemProps {
-  children: React.ReactNode;
-}
-
-export default function DropdownMenuItem({ children }: DropdownItemProps) {
+export default function DropdownMenuItem({ children }: { children: React.ReactNode }) {
   return (
     <li className='cursor-pointer rounded-sm px-4 py-1 font-md-regular text-gray-700 hover:bg-violet-500-8 hover:text-violet-500'>
       {children}
