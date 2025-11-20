4c106fc29cf19398c31f777c75ed8d80e66f1e66
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-20 09:38:17 +0900

    [Chore/33] axios 패키지 설치, instance 설정 (#34)

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,1 +1,11 @@
-// TODO: axios 인스턴스 정의 필요
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default api;
+
+// TODO: 인터셉터로 Authorization 헤더에 토큰 전달하도록 구현
