eabf72f6a2d16b16d2e3b83e910b097aeaa00b64
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-21 20:53:19 +0900

    ✨ Feat: 태그 컴포넌트 태그 삭제 버튼 추가

--- src/components/dashboard/Tag.tsx
+++ src/components/dashboard/Tag.tsx
@@ -1,8 +1,9 @@
 import { cva } from 'class-variance-authority';
+import Icons from '@/assets/icons';
 import type { ColorTypes } from '@/constants/color';
 
 const tagStyles = cva(
-  'inline-block px-1.5 py-1 rounded-sm font-xs-regular sm:py-0.5 sm:font-md-regular',
+  'w-fit flex items-center px-1.5 py-1 rounded-sm font-xs-regular sm:py-0.5 sm:font-md-regular gap-2',
   {
     variants: {
       color: {
@@ -19,17 +20,29 @@ const tagStyles = cva(
 interface TagProps {
   children: React.ReactNode;
   color: ColorTypes;
+  onDelete?: () => void;
 }
 
 /**
- * Tag 컴포넌트
+ * ### Tag 컴포넌트
  * 대시보드 카드, 태그 인풋에서 사용되는 컴포넌트입니다.
  *
  * @example
  * ```tsx
  * <Tag color='orange'>태그</Tag>
+ * // 태그를 삭제해야 하는 경우 onDelete 메서드를 넘겨주면 됩니다.
+ * <Tag color='orange' onDelete={onClick}>태그</Tag>
  * ```
  */
-export default function Tag({ children, color }: TagProps) {
-  return <span className={tagStyles({ color })}>{children}</span>;
+export default function Tag({ children, color, onDelete }: TagProps) {
+  return (
+    <div className={tagStyles({ color })}>
+      <span>{children}</span>
+      {onDelete && (
+        <button className='h-4 w-4 cursor-pointer text-gray-500' type='button' onClick={onDelete}>
+          <Icons.Close width={16} height={16} />
+        </button>
+      )}
+    </div>
+  );
 }
