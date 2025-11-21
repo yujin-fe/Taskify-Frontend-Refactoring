c5b98a321b17516ed382882481f30155304ec841
apple <apple@apples-MacBook-Air.local>
2025-11-21 16:19:51 +0900

    ♻️ Refactor: home..tsx 테스트 코드 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,3 @@
-import { ImageUpload } from '@/components/common/ImageUploader';
-
-export default function Example() {
-  return (
-    <div>
-      <div>
-        <p>Small</p>
-        <ImageUpload size='Small' />
-      </div>
-
-      <div>
-        <p>Large</p>
-        <ImageUpload size='Large' />
-      </div>
-    </div>
-  );
+export default function Home() {
+  return <div>메인 페이지~</div>;
 }
