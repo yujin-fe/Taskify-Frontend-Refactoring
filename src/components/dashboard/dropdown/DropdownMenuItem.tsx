36c43bf6b220a2765a7a43090965c17a235bd231
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-22 10:59:33 +0900

    [♻️ Refactor/74] DropdownMenuItem, DropdownMenuList 컴포넌트 수정 (#77)

--- src/components/dashboard/dropdown/DropdownMenuItem.tsx
+++ src/components/dashboard/dropdown/DropdownMenuItem.tsx
@@ -1,6 +1,25 @@
-export default function DropdownMenuItem({ children }: { children: React.ReactNode }) {
+import { useContext } from 'react';
+import { DropdownMenuContext } from '@/context/dropdownMenuContext';
+
+interface DropdownMenuItemProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+export default function DropdownMenuItem({ children, onClick }: DropdownMenuItemProps) {
+  const { handleToggleOpen } = useContext(DropdownMenuContext);
+
+  const handleClickMenuItem = () => {
+    handleToggleOpen();
+    onClick();
+  };
+
   return (
-    <li className='dropdown-menu-item-base py-1 text-center font-md-regular hover:bg-violet-500-8 hover:text-violet-500'>
+    <li
+      role='menuitem'
+      tabIndex={0}
+      className='dropdown-menu-item-base py-1 text-center font-md-regular hover:bg-violet-500-8 hover:text-violet-500'
+      onClick={handleClickMenuItem}>
       {children}
     </li>
   );
