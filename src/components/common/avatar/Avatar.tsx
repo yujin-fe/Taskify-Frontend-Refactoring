15678be7e848a951b9d4ae96b1bb8e898d8cf712
yujin-fe <hyujin1224@gmail.com>
2025-11-23 00:16:52 +0900

    [♻️ Refactor/78] 아바타 컴포넌트 컴파운드 패턴으로 리팩토링 (#88)

---
+++ src/components/common/avatar/Avatar.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import AvatarFallback from '@/components/common/avatar/AvatarFallback';
+import AvatarImg from '@/components/common/avatar/AvatarImg';
+import { AvatarContext } from '@/context/avatarContext';
+import type { UserMe } from '@/types/userMe';
+import { cn } from '@/utils/cn';
+
+interface AvatarProps {
+  children: React.ReactNode;
+  size: 's' | 'm';
+  user: UserMe;
+}
+
+/**
+ * @example
+ * <Avatar size="s" user={user} key={user.id}>
+ *  <Avatar.Img />
+ *  <Avatar.Fallback />
+ * </Avatar>
+ */
+
+export default function Avatar({ children, size, user }: AvatarProps) {
+  const [imageError, setImageError] = useState(false);
+  const AvatarStyle = cn('rounded-full overflow-hidden', {
+    'w-[24px] h-[24px] ': size === 's',
+    'w-[38px] h-[38px] border-2 border-gray-0': size === 'm',
+  });
+  //children 중 AvatarImg 태그
+  const img = React.Children.toArray(children).find(
+    (child) => React.isValidElement(child) && child.type === AvatarImg
+  );
+  //children 중 AvatarFallback 태그
+  const fallback = React.Children.toArray(children).find(
+    (child) => React.isValidElement(child) && child.type === AvatarFallback
+  );
+  return (
+    <AvatarContext value={{ ...user, setImageError }}>
+      <div className={AvatarStyle}>{user.profileImageUrl && !imageError ? img : fallback}</div>
+    </AvatarContext>
+  );
+}
+
+Avatar.Img = AvatarImg;
+
+Avatar.Fallback = AvatarFallback;
--- src/components/common/avatar/AvatarFallback.tsx
+++ src/components/common/avatar/AvatarFallback.tsx
@@ -1,4 +1,6 @@
 import { cva } from 'class-variance-authority';
+import { useContext } from 'react';
+import { AvatarContext } from '@/context/avatarContext';
 import { getMonogram, getProfileColorForId } from '@/utils/avatar';
 
 const AvatarFallbackStyle = cva('flex items-center justify-center w-full h-full text-gray-0', {
@@ -13,12 +15,8 @@ const AvatarFallbackStyle = cva('flex items-center justify-center w-full h-full 
   },
 });
 
-interface AvatarFallbackProps {
-  nickname: string;
-  id: number;
-}
-
-export default function AvatarFallback({ nickname, id }: AvatarFallbackProps) {
+export default function AvatarFallback() {
+  const { nickname, id } = useContext(AvatarContext);
   const color = getProfileColorForId(id);
 
   return <div className={AvatarFallbackStyle({ color })}>{getMonogram(nickname)}</div>;
