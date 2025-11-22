84fdab750b968396216bb0f112b794f4ec3e4aba
JongIn Hong <ghdwhddls99@naver.com>
2025-11-22 11:11:01 +0900

    ♻️ Refactor: PageIndicator 리뷰 반영

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,3 @@
-import { PageIndicator } from '@/components/common/PageIndicator';
-import PageNation from '@/components/common/PageNation';
-
 export default function Home() {
-  const handlePrev = () => {
-    // API 호출 로직 - 이전 페이지로 이동
-  };
-
-  const handleNext = () => {
-    // API 호출 로직 - 다음 페이지로 이동
-  };
-
-  return (
-    <div>
-      <p>메인 페이지~</p>
-      <div className='mt-8 flex items-center justify-center gap-4'>
-        <PageIndicator currentPage={2} totalPages={10} />
-        <PageNation
-          onPrev={handlePrev}
-          onNext={handleNext}
-          prevDisabled={false}
-          nextDisabled={false}
-        />
-      </div>
-    </div>
-  );
+  return <div>메인 페이지~</div>;
 }
