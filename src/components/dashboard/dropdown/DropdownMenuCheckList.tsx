35a19a1c9131e77864997245420ffa0e99e0f3fc
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-22 19:10:37 +0900

    🐛 Fix: flex 레이아웃 제거

--- src/components/dashboard/dropdown/DropdownMenuCheckList.tsx
+++ src/components/dashboard/dropdown/DropdownMenuCheckList.tsx
@@ -6,7 +6,7 @@ export default function DropdownMenuCheckList({ children }: { children: React.Re
 
   return (
     isOpen && (
-      <ul className='absolute top-[calc(100%+8px)] left-0 z-1000 w-full flex-col justify-center rounded-md border border-gray-300 bg-gray-0'>
+      <ul className='absolute top-[calc(100%+8px)] left-0 z-1000 w-full rounded-md border border-gray-300 bg-gray-0'>
         {children}
       </ul>
     )
