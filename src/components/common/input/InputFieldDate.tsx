d437eb04a8ff75c5a95cbeed40d61eba730a9af1
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-24 01:29:58 +0900

    📝 Docs: todo 주석 추가

--- src/components/common/input/InputFieldDate.tsx
+++ src/components/common/input/InputFieldDate.tsx
@@ -9,6 +9,7 @@ export default function InputFieldDate({ placeholder }: { placeholder: string })
     <>
       <Icons.Calendar className='absolute text-gray-400' />
       <div className={cn('h-full w-full pl-[30px] text-gray-400', value && 'text-black')}>
+        {/* TODO: value format 유틸 함수 구현 후 적용 */}
         {value ? value : placeholder}
       </div>
       <input
