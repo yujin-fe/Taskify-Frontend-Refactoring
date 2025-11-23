e082da6a66529b486e5dfb2959d3b0a06108899d
yujin-fe <hyujin1224@gmail.com>
2025-11-24 02:19:36 +0900

    🐛 Fix: 경로 임시 수정

--- src/components/common/modal/FormModalFooter.tsx
+++ src/components/common/modal/FormModalFooter.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import Button from '@/components/common/Button';
-import { FormModalContext } from '@/context/FormModalContext';
+import { FormModalContext } from '@/context/adasfaformModalContext';
 export default function FormModalFooter({
   cancleButton,
   submitButton,
