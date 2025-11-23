46d5a4c29260462cf301862756712529cfe331b1
apple <apple@apples-MacBook-Air.local>
2025-11-23 18:56:04 +0900

    ♻️ Refactor: div 속성 제거

--- src/components/common/modal/BaseModalFrame.tsx
+++ src/components/common/modal/BaseModalFrame.tsx
@@ -33,8 +33,7 @@ export default function BaseModalFrame({
 }: BaseModalFrameProps) {
   return (
     <ModalPortal>
-      <div className='modal-dimmed' />
-      <div className='fixed inset-0 flex items-center justify-center'>
+      <div className='modal-dimmed'>
         <div
           className={cn(
             'flex flex-col items-center justify-center bg-gray-0 text-center font-lg-medium text-gray-700 sm:w-[368px] sm:gap-8 sm:pt-[40px] sm:pb-[40px] sm:font-xl-medium',
