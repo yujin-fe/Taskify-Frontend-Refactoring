9e1de944bd1b792c9e17c7d7ba996e51386d2771
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-23 16:57:46 +0900

    ♻️ Refactor: dropdownContext 분리

--- src/context/dropdownMenuContext.ts
+++ src/context/dropdownMenuContext.ts
@@ -1,14 +1,26 @@
 import { createContext } from 'react';
 
-interface DropdownMenuContextType {
+/**
+ * 기본 드롭다운 메뉴(드롭다운 1) 컨텍스트 타입
+ */
+interface BaseDropdownMenuContextType {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
-  selectedValue?: string;
-  setSelectedValue?: (value: string) => void;
+}
+
+/**
+ * 검색 가능한 드롭다운 메뉴(드롭다운 2) 컨텍스트 타입
+ */
+interface SearchableDropdownMenuContextType extends BaseDropdownMenuContextType {
+  selectedValue: string;
+  setSelectedValue: (value: string) => void;
   searchQuery: string;
   setSearchQuery: (query: string) => void;
   selectedNode: null | React.ReactNode;
   setSelectedNode: (node: null | React.ReactNode) => void;
 }
 
-export const DropdownMenuContext = createContext<DropdownMenuContextType | null>(null);
+export const BaseDropdownMenuContext = createContext<BaseDropdownMenuContextType | null>(null);
+
+export const SearchableDropdownMenuContext =
+  createContext<SearchableDropdownMenuContextType | null>(null);
