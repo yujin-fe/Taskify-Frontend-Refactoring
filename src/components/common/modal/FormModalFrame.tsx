09f0a29583d95784176b0d5f9b94436985e75b8d
yujin-fe <hyujin1224@gmail.com>
2025-11-23 20:18:05 +0900

    📝 Docs: 주석에 설명 추가

--- src/components/common/modal/FormModalFrame.tsx
+++ src/components/common/modal/FormModalFrame.tsx
@@ -31,11 +31,28 @@ interface FormModalFrameProps extends VariantProps<typeof FormModalFrameStyle> {
   setOnModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 /**
+ * padding 값에 따라서 크기를 지정해주세요
+ *
+ * lg-패딩 32px
+ *
+ * md-패딩 24px
+ *
+ * sm-패딩 24px 16px
+ *
+ * gap은 타이틀과 폼(children) 폼과 버튼 사이의 픽셀을 골라주세요
+ *
+ * leftButton을 클릭하면 onModal이 false로 변경됩니다
+ *
+ * rightButton을 클릭하면 폼이 제출됩니다
  *
  * @example
+ * const [onModal, setOnModal] = useState(false)
+ * {onModal &&(
  * <FormModalFrame title="할일 생성" leftButton="취소" rightButton="생성" size="lg" gap={32}>
  *  <NewTaskForm/>
  * </FormModalFrame>
+ * )}
+ * <button onClick={()=>setOnModal(true)}>버튼</button>
  */
 
 export default function FormModalFrame({
