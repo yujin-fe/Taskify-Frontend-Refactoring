394272d4ba7ff43c9698a6cfffc7c42de4c7fde5
yujin-fe <hyujin1224@gmail.com>
2025-11-24 22:58:05 +0900

    ♻️ Refactor: 새로고침시 모달이 닫히지 않도록 useSearchParams로 쿼리스트링 관리하도록 수정

--- src/components/common/modal/FormModal.tsx
+++ src/components/common/modal/FormModal.tsx
@@ -34,25 +34,27 @@ interface FormModalProps extends VariantProps<typeof FormModalFrameStyle> {
 }
 
 /**
- * <form> 태그의 위치에 따라 FormModal.Body와 FormModal.Footer를 사용해주세요.
- *
- * padding 값은 size에 따라 자동으로 적용됩니다.
+ * 예시의 <form> 태그의 위치에 따라 FormModal.Body와 FormModal.Footer를 사용해주세요.
  *
  * - size="lg": 기본 패딩 32px (모바일 24px)
  * - size="md": 기본 패딩 24px (모바일 24px 16px)
  *
  * gap은 Title ↔ Body 사이 간격을 의미합니다.
  *
- * cancleButton을 클릭하면 modal.handleModalClose()가 실행되어 isOpen이 false가 됩니다.
+ * cancelButton을 클릭하면 isOpen이 false가 됩니다.
  * submitButton을 클릭하면 form이 제출됩니다.
  *
- * ⚠️ 아래 예시처럼 `modal`(useModal의 반환 객체)를 반드시 prop으로 전달해주세요.
+ * ⚠️ 아래 예시처럼 useModal의 반환 객체를 반드시 prop으로 전달해주세요.
+ * 인수로는 모달의 이름을 string형식으로 전달하시면 됩니다.
+ * 각각의 모달마다 고유한 이름을 부여하기 위함이니 모달마다 다른 이름을 사용해주세요.
+ * - ex)newTaskModal, addColumnModal
+ *
  *
  * @example
- * const modal = useModal();
+ * const newTaskModal = useModal("newTaskModal");
  *
- * {modal.isOpen && (
- *   <FormModal size="lg" gap={32} modal={modal}>
+ * {newTaskModal.isOpen && (
+ *   <FormModal size="lg" gap={32} modal={newTaskModal}>
  *     <FormModal.Title title="할일 생성" />
  *     <form>
  *       <FormModal.Body>
@@ -63,7 +65,7 @@ interface FormModalProps extends VariantProps<typeof FormModalFrameStyle> {
  *   </FormModal>
  * )}
  *
- * <button onClick={modal.handleModalOpen}>열기</button>
+ * <button onClick={newTaskModal.handleModalOpen}>열기</button>
  */
 
 export default function FormModal({ size, gap, children, className, modal }: FormModalProps) {
--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,14 +1,19 @@
 import { useState } from 'react';
+import { useSearchParams } from 'react-router';
 
-export const useModal = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export const useModal = (modalName: string) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initialOpen = searchParams.get(modalName) === 'true';
+  const [isOpen, setIsOpen] = useState(initialOpen);
 
   const handleModalOpen = () => {
     setIsOpen(true);
+    setSearchParams({ [modalName]: 'true' });
   };
 
   const handleModalClose = () => {
     setIsOpen(false);
+    setSearchParams({});
   };
 
   return { isOpen, handleModalOpen, handleModalClose };
