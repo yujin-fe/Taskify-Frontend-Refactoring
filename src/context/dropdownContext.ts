038ba82f668f8d3f97a2f7c0227ebc20dd87df81
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-24 12:04:56 +0900

    ♻️ Refactor: context 파일 분리

--- src/context/dropdownContext.ts
+++ src/context/dropdownContext.ts
@@ -1,25 +1,10 @@
 import { createContext } from 'react';
 
-/**
- * 기본 드롭다운 메뉴(드롭다운 1) 컨텍스트 타입
- */
-interface BasicDropdownContextType {
+interface DropdownContextType {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 }
 
-/**
- * 검색 가능한 드롭다운 메뉴(드롭다운 2) 컨텍스트 타입
- */
-interface SearchableDropdownContextType extends BasicDropdownContextType {
-  selectedValue: string;
-  setSelectedValue: (value: string) => void;
-  searchQuery: string;
-  setSearchQuery: (query: string) => void;
-  selectedNode: null | React.ReactNode;
-  setSelectedNode: (node: null | React.ReactNode) => void;
-}
+const DropdownContext = createContext<DropdownContextType | null>(null);
 
-export const BasicDropdownContext = createContext<BasicDropdownContextType | null>(null);
-
-export const SearchableDropdownContext = createContext<SearchableDropdownContextType | null>(null);
+export default DropdownContext;
