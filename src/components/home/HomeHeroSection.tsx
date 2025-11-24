74f66b3c5dd61cfe52e8b4ca08fa734a52f78a38
aahreum <cocoding420@gmail.com>
2025-11-25 01:54:31 +0900

    🐛 Fix: HomeHeroSection 스타일 오류 수정

--- src/components/home/HomeHeroSection.tsx
+++ src/components/home/HomeHeroSection.tsx
@@ -8,7 +8,7 @@ export default function HomeHeroSection() {
     <section className='flex flex-col items-center gap-12 px-[40px]'>
       <img src={images.mainVisual} className='w-full lg:w-[722px]' alt='' />
       <Title
-        className='flex flex-col items-center gap-1.5 text-[40px] leading-[48px] tracking-[-2px] text-gray-0 md:flex-row md:gap-6 md:text-[56px] md:leading-[100px] lg:gap-7 lg:text-[76px]'
+        className='flex flex-col items-center gap-1.5 text-[40px] leading-[48px] tracking-[-2px] whitespace-nowrap text-gray-0 md:flex-row md:gap-6 md:text-[56px] md:leading-[100px] lg:gap-7 lg:text-[76px]'
         weight={'bold'}>
         <span>새로운 일정 관리</span>
         <span className='font-montserrat text-[42px] tracking-[-1px] text-primary md:text-[76px] lg:text-[90px]'>
