8981e0f3a73acf3fb69f7ed7e9545f9825ce46f3
aahreum <cocoding420@gmail.com>
2025-11-25 18:10:42 +0900

    🎨 Style: hover, 상단 고정 추가

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router';
 import { Logo } from '@/components/common/Logo';
 
+const linkStyle = 'rounded-[4px] py-1.5 px-2 md:px-3 md:py-2 hover:bg-gray-700';
+
 export default function HomeHeader() {
   return (
-    <header className='mx-auto flex h-[70px] max-w-[1760px] items-center justify-between'>
-      <Logo color='base' className='hidden sm:flex' />
-      <Logo color='base' size='Small' className='sm:hidden' />
-      <nav className='flex gap-[24px] text-gray-0 sm:gap-[36px]'>
-        <Link to='/login'>로그인</Link>
-        <Link to='/signup'>회원가입</Link>
-      </nav>
+    <header className='sticky top-0 w-full bg-gray-900'>
+      <div className='mx-auto flex h-[70px] max-w-[1760px] items-center justify-between'>
+        <Logo color='base' className='hidden sm:flex' />
+        <Logo color='base' size='Small' className='sm:hidden' />
+        <nav className='mr-[-8px] flex gap-[8px] text-gray-0 sm:mr-[-12px] sm:gap-[12px]'>
+          <Link to='/login' className={linkStyle}>
+            로그인
+          </Link>
+          <Link to='/signup' className={linkStyle}>
+            회원가입
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
