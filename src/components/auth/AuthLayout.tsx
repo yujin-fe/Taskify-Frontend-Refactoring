907dc303934132139b7313dc8fe887419a6a152a
aahreum <cocoding420@gmail.com>
2025-11-25 23:07:57 +0900

    🎨 Style: padding 추가

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 export default function AuthLayout() {
   return (
-    <div className='min-h-[100dvh] bg-base pt-[80px] sm:pt-[160px]'>
+    <div className='min-h-[100dvh] bg-base px-[12px] py-[80px] sm:py-[160px]'>
       <main className='mx-auto flex w-full max-w-[560px] flex-col items-center'>
         <Outlet />
       </main>
