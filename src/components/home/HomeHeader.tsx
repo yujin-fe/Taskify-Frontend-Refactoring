1848498056643b1b7cab00b5aeca51aaba96f463
aahreum <cocoding420@gmail.com>
2025-11-25 16:08:35 +0900

    ✨ Feat: HomeHeader 컴포넌트 구현 및 적용

---
+++ src/components/home/HomeHeader.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router';
+import { Logo } from '@/components/common/Logo';
+
+export default function HomeHeader() {
+  return (
+    <header className='mx-auto flex h-[70px] max-w-[1760px] items-center justify-between'>
+      <Logo color='base' className='hidden sm:flex' />
+      <Logo color='base' size='Small' className='sm:hidden' />
+      <nav className='flex gap-[36px] text-gray-0'>
+        <Link to='/login'>로그인</Link>
+        <Link to='/signup'>회원가입</Link>
+      </nav>
+    </header>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import HomeFooter from '@/components/home/HomeFooter';
+import HomeHeader from '@/components/home/HomeHeader';
 import HomeHeroSection from '@/components/home/HomeHeroSection';
 import HomeIntroSection from '@/components/home/HomeIntroSection';
 import HomeServiceDetailsSection from '@/components/home/HomeServiceDetailsSection';
@@ -6,6 +7,7 @@ import HomeServiceDetailsSection from '@/components/home/HomeServiceDetailsSecti
 export default function Home() {
   return (
     <div className='bg-gray-900 px-[16px] sm:px-[40px]'>
+      <HomeHeader />
       <main className='mx-auto max-w-[1200px] pt-[94px]'>
         <HomeHeroSection />
         <HomeIntroSection />
