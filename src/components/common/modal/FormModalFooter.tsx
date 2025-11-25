07499462b6d6d49cc15c14e0e206862e54cf995e
yujin-fe <hyujin1224@gmail.com>
2025-11-25 15:16:40 +0900

    ♻️ Refactor: 버튼 컴포넌트를 children으로 받도록 Footer 수정

--- src/components/common/modal/FormModalFooter.tsx
+++ src/components/common/modal/FormModalFooter.tsx
@@ -1,27 +1,5 @@
-import { useContext } from 'react';
-import Button from '@/components/common/Button';
-import { FormModalContext } from '@/context/formModalContext';
-export default function FormModalFooter({
-  cancelButton,
-  submitButton,
-}: {
-  cancelButton: string;
-  submitButton: string;
-}) {
-  const { handleModalClose } = useContext(FormModalContext);
+export default function FormModalFooter({ children }: { children: React.ReactNode }) {
   return (
-    <div className={'flex w-full max-sm:gap-[7px] sm:gap-2'}>
-      <Button
-        size='lg'
-        theme={'outlined'}
-        onClick={() => {
-          handleModalClose();
-        }}>
-        {cancelButton}
-      </Button>
-      <Button size='lg' theme={'primary'} type='submit'>
-        {submitButton}
-      </Button>
-    </div>
+    <div className={'flex w-full max-w-[520px] gap-[7px] sm:w-[520px] sm:gap-2'}>{children}</div>
   );
 }
