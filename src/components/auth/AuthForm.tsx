99cff620a4d0a9204765db1e671fdb7895ca8964
aahreum <cocoding420@gmail.com>
2025-11-26 01:43:57 +0900

    ✨ Feat: AuthForm 컴포넌트 구현

---
+++ src/components/auth/AuthForm.tsx
@@ -0,0 +1,17 @@
+interface AuthFormProps {
+  onSubmit: () => void;
+  children: React.ReactNode;
+}
+
+export default function AuthForm({ onSubmit, children }: AuthFormProps) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <form className='mb-[24px] flex w-full flex-col gap-[16px]' onSubmit={handleSubmit} noValidate>
+      {children}
+    </form>
+  );
+}
