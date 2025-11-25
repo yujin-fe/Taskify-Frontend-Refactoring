0e5d1203a9f071d811f05bdc3f3f31f28f294da3
yujin-fe <hyujin1224@gmail.com>
2025-11-25 15:50:28 +0900

    ♻️ Refactor: 스크롤 방지

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router';
 
 export const useModal = (modalName: string) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const isOpen = searchParams.get(modalName) === 'true';
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('modal-open');
+    } else {
+      document.body.classList.remove('modal-open');
+    }
+    return () => {
+      document.body.classList.remove('modal-open');
+    };
+  }, [isOpen]);
 
   const handleModalOpen = () => {
     setSearchParams((prev) => {
