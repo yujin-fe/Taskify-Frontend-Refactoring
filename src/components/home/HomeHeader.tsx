af70609bad8fd50c74b2504a161bc7a761f760fc
aahreum <cocoding420@gmail.com>
2025-11-25 19:49:14 +0900

    ♻️ Refactor: picture로 로고 이미지 반응형 대응

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router';
-import { Logo } from '@/components/common/Logo';
+import logos from '@/assets/logos';
 
 const linkStyle = 'rounded-[4px] py-1.5 px-2 md:px-3 md:py-2 hover:bg-gray-700';
 
@@ -7,8 +7,14 @@ export default function HomeHeader() {
   return (
     <header className='sticky top-0 w-full bg-gray-900'>
       <div className='mx-auto flex h-[70px] max-w-[1760px] items-center justify-between'>
-        <Logo color='base' className='hidden sm:flex' />
-        <Logo color='base' size='Small' className='sm:hidden' />
+        <h1>
+          <Link to='/' aria-label='Taskify 홈으로 이동'>
+            <picture>
+              <source width={24} media='(max-width: 639px)' srcSet={logos.smallUrl} />
+              <img width={121} src={logos.mediumUrl} alt='Taskify logo' />
+            </picture>
+          </Link>
+        </h1>
         <nav className='mr-[-8px] flex gap-[8px] text-gray-0 sm:mr-[-12px] sm:gap-[12px]'>
           <Link to='/login' className={linkStyle}>
             로그인
