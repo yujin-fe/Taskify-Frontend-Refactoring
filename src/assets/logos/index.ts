d73480dabf997dfcc4cf0428ba5b593cc68950ce
aahreum <cocoding420@gmail.com>
2025-11-25 19:55:26 +0900

    📝 Docs: 주석 수정

--- src/assets/logos/index.ts
+++ src/assets/logos/index.ts
@@ -1,6 +1,6 @@
 /**
  * Large, Medium, Small -> SVG 컴포넌트
- * mediumUrl, smallUrl -> svg url로 활용 (이미지 태그로 사용할 때)
+ * mediumUrl, smallUrl -> url로 활용 (이미지 태그로 사용할 때)
  */
 import Large from '@/assets/logos/logo-lg.svg';
 import mediumUrl from '@/assets/logos/logo-m.png';
