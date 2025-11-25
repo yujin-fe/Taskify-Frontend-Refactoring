f8f3d8a150428a9440736b54afd81d1e4d175c1a
yujin-fe <hyujin1224@gmail.com>
2025-11-25 21:11:15 +0900

    🐛 Fix: 버튼 padding-top 설정

--- src/components/common/modal/FormModalFooter.tsx
+++ src/components/common/modal/FormModalFooter.tsx
@@ -1,5 +1,16 @@
-export default function FormModalFooter({ children }: { children: React.ReactNode }) {
+import { cn } from '@/utils/cn';
+
+export default function FormModalFooter({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <div className={'flex w-full max-w-[520px] gap-[7px] sm:w-[520px] sm:gap-2'}>{children}</div>
+    <div
+      className={cn('flex w-full max-w-[520px] gap-[7px] pt-6 sm:w-[520px] sm:gap-2', className)}>
+      {children}
+    </div>
   );
 }
