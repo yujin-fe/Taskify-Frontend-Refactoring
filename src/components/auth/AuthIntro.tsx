c48bbc6f738176cd20d5e29922cee4c21879b3cc
aahreum <cocoding420@gmail.com>
2025-11-25 23:35:38 +0900

    📝 Docs: 컴포넌트 설명 주석 추가

--- src/components/auth/AuthIntro.tsx
+++ src/components/auth/AuthIntro.tsx
@@ -1,5 +1,12 @@
 import { Logo } from '@/components/common/Logo';
 
+/**
+ * ### AuthIntro
+ * - Logo와 안내 문구를 함께 보여주는 컴포넌트 입니다.
+ *
+ * @example
+ * <AuthIntro comment="첫 방문을 환영합니다!" />
+ */
 export default function AuthIntro({ comment }: { comment: string }) {
   return (
     <div className='mb-[36px] flex flex-col items-center gap-[8px] sm:mb-[30px] sm:gap-[10px]'>
--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from 'react-router';
 
+/**
+ * ### AuthLayout
+ * - 라우터 설정에 이미 적용되어 있어서 적용하실 필요 없습니다!
+ * - Login, Signup에 공통으로 적용되는 레이아웃 컴포넌트입니다.
+ */
 export default function AuthLayout() {
   return (
     <div className='min-h-[100dvh] bg-base px-[12px] py-[80px] sm:py-[160px]'>
