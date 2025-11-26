b12adf9c92a80ccb651c4229c365c96f36bc1aef
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-26 09:56:14 +0900

    [🐛 Bug/160] profilefallback 배경 컬러 수정 (#161)

--- src/components/common/avatar/AvatarFallback.tsx
+++ src/components/common/avatar/AvatarFallback.tsx
@@ -6,11 +6,11 @@ import { getMonogram, getProfileColorForId } from '@/utils/avatar';
 const AvatarFallbackStyle = cva('flex items-center justify-center w-full h-full text-gray-0', {
   variants: {
     color: {
-      orange: 'bg-orange-500/80',
-      blue: 'bg-blue-500/80',
-      green: 'bg-green-500/80',
-      pink: 'bg-pink-500/80',
-      purple: 'bg-purple-500/80',
+      orange: 'bg-profile-orange',
+      blue: 'bg-profile-blue',
+      green: 'bg-profile-green',
+      pink: 'bg-profile-pink',
+      purple: 'bg-profile-purple',
     },
   },
 });
