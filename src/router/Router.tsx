1ca16ca4bae0ec74677a3ba162f52e1d8b777f9e
aahreum <cocoding420@gmail.com>
2025-11-27 00:57:38 +0900

    ✨ Feat: 라우터 연결

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import AuthLayout from '@/components/auth/AuthLayout';
+import DashboardDetail from '@/pages/DashboardDetail';
+import DashboardEdit from '@/pages/DashboardEdit';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
+import MyDashboard from '@/pages/MyDashboard';
+import Mypage from '@/pages/Mypage';
 import Signup from '@/pages/Signup';
 
 export default function Router() {
@@ -12,6 +16,13 @@ export default function Router() {
         <Route element={<AuthLayout />}>
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+        </Route>
+        {/* 레이아웃 컴포넌트 연결 */}
+        <Route>
+          <Route path='/mydashboard' element={<MyDashboard />} />
+          <Route path='/dashboard/:dashboardId' element={<DashboardDetail />} />
+          <Route path='/dashboard/:dashboardId/edit' element={<DashboardEdit />} />
+          <Route path='/mypage' element={<Mypage />} />
         </Route>
       </Routes>
     </BrowserRouter>
