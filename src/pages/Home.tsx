4e3603f9c65cd9de083ab4760cf65e47ff966860
apple <apple@apples-MacBook-Air.local>
2025-11-26 18:14:13 +0900

    ✨ Feat: 홈 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,24 @@
-import DashboardItem from '@/components/dashboard/table/DashboardMembersItem';
-
-interface UserMe {
-  id: number;
-  nickname: string;
-  profileImageUrl: string | null;
-  email: string;
-  createdAt: string;
-  updatedAt: string;
-}
+import HomeFooter from '@/components/home/HomeFooter';
+import HomeHeader from '@/components/home/HomeHeader';
+import HomeHeroSection from '@/components/home/HomeHeroSection';
+import HomeIntroSection from '@/components/home/HomeIntroSection';
+import HomeServiceDetailsSection from '@/components/home/HomeServiceDetailsSection';
 
 export default function Home() {
-  const users: UserMe[] = [
-    {
-      id: 101,
-      nickname: '김00',
-      profileImageUrl: null,
-      email: 'alice@example.com',
-      createdAt: '2023-01-01',
-      updatedAt: '2023-11-26',
-    },
-    {
-      id: 102,
-      nickname: '정00',
-      profileImageUrl: 'http://example.com/bob.jpg',
-      email: 'bob@example.com',
-      createdAt: '2023-01-05',
-      updatedAt: '2023-11-26',
-    },
-  ];
-
-  const handleDelete = (userId: number) => {
-    const deletedUser = users.find((u) => u.id === userId);
-
-    alert(`사용자 ID ${userId} (${deletedUser?.nickname} 님)를 삭제하시겠습니까?`);
-  };
+  /**
+   * TODO: 로그인 되었을 때 /dashboard/{dashboardId}로,
+   * dashboard가 없다면 /dashboard 리다이렉트 되도록 구현
+   */
 
   return (
-    <div className='space-y-2 p-4'>
-      {users.map((user) => (
-        <DashboardItem
-          key={user.id}
-          user={user}
-          onDelete={handleDelete}
-          className='border-b pb-2'
-        />
-      ))}
+    <div className='bg-gray-900 px-[16px] sm:px-[40px]'>
+      <HomeHeader />
+      <main className='mx-auto max-w-[1200px] pt-[94px]'>
+        <HomeHeroSection />
+        <HomeIntroSection />
+        <HomeServiceDetailsSection />
+      </main>
+      <HomeFooter />
     </div>
   );
 }
