77a880ab0c3e2f9c934af862d15313a74b9a7d31
aahreum <cocoding420@gmail.com>
2025-11-27 00:59:41 +0900

    🚚 Rename: pages 하위로 이동

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-import AuthLayout from '@/components/auth/AuthLayout';
+import AuthLayout from '@/pages/AuthLayout';
 import DashboardDetail from '@/pages/DashboardDetail';
 import DashboardEdit from '@/pages/DashboardEdit';
 import Home from '@/pages/Home';
