b261235fd88e7cdf5f4148d352b711c7060772ec
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-26 19:30:45 +0900

    [🐛 Bug/144] InputLabel을 Label로 분리 (#162)

--- src/components/common/input/InputLabel.tsx
+++ src/components/common/input/InputLabel.tsx
@@ -1,21 +1,7 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import Label, { type LabelProps } from '@/components/common/Label';
 import useInputContext from '@/hooks/useInputContext';
-import { cn } from '@/utils/cn';
 
-interface InputLabelProps extends ComponentPropsWithoutRef<'label'> {
-  required?: boolean; // true면 '*' 표시
-}
-/**
- * InputLabel 컴포넌트 (공통 라벨)
- *
- * - 기본 HTML <label> 태그를 기반으로 만든 공통 컴포넌트입니다.
- * - children에는 문자열, 아이콘, 다양한 JSX 요소를 자유롭게 넣을 수 있습니다.
- *
- * 사용 예시:
- *   <InputLabel className="text-sm font-medium">
- *     아이디
- *   </InputLabel>
- */
+type InputLabelProps = Omit<LabelProps, 'htmlFor'>;
 
 export default function InputLabel({
   children,
@@ -26,9 +12,8 @@ export default function InputLabel({
   const { id } = useInputContext();
 
   return (
-    <label htmlFor={id} className={cn('text-gray-700', className)} {...props}>
+    <Label htmlFor={id} className={className} required={required} {...props}>
       {children}
-      {required && <span className='font-lg-regular text-violet-500'>*</span>}
-    </label>
+    </Label>
   );
 }
