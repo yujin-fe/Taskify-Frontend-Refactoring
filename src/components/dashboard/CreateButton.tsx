ac57588703640e28cc728c9eac52722626551897
aahreum <cocoding420@gmail.com>
2025-11-27 11:44:42 +0900

    ✨ Feat: CreateButton 컴포넌트 구현

---
+++ src/components/dashboard/CreateButton.tsx
@@ -0,0 +1,28 @@
+import Icons from '@/assets/icons';
+import { cn } from '@/utils/cn';
+
+interface CreateButtonProps {
+  onClick: () => void;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export default function CreateButton({ onClick, className, children }: CreateButtonProps) {
+  return (
+    <button
+      type='button'
+      className={cn(
+        'flex h-[32px] w-full cursor-pointer items-center justify-center rounded-[6px] border border-gray-300 bg-gray-0 sm:h-[40px]',
+        children && 'gap-[12px] text-gray-700 sm:h-[70px]',
+        className
+      )}
+      onClick={onClick}>
+      {children}
+      <Icons.Plus
+        width={22}
+        height={22}
+        className='rounded-[4px] bg-violet-500-8 p-[3px] text-primary'
+      />
+    </button>
+  );
+}
