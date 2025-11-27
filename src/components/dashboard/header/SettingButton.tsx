7c750850043a4f6c2456eef88d9545a3ff892149
JongIn Hong <ghdwhddls99@naver.com>
2025-11-27 14:44:43 +0900

    ✨ Feat: SettingButton 컴포넌트 구현

---
+++ src/components/dashboard/header/SettingButton.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router';
+import Icons from '@/assets/icons';
+import Button from '@/components/common/Button';
+
+export default function SettingButton() {
+  return (
+    <Button
+      as={Link}
+      to='/manage'
+      theme='outlined'
+      size='lg'
+      className='h-[40px] w-[88px] gap-2 sm:h-[40px] sm:w-[88px]'>
+      <Icons.Setting className='h-[20px] w-[20px]' aria-hidden />
+      <span className='font-lg-medium text-gray-300'>관리</span>
+    </Button>
+  );
+}
