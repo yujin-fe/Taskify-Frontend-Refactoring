a0382f2ab14bdc4d8ac3ef35f7a224505c07e035
aahreum <cocoding420@gmail.com>
2025-11-27 18:10:46 +0900

    🐛 Fix: 타입명 수정

--- src/lib/apis/dashboards.ts
+++ src/lib/apis/dashboards.ts
@@ -1,12 +1,12 @@
 import type { ColorHex } from '@/constants/color';
 import { api } from '@/lib/axios';
 
-interface createDashboardType {
+interface CreateDashboardType {
   title: string;
   color: ColorHex;
 }
 
-export const createDashboard = async (reqBody: createDashboardType) => {
+export const createDashboard = async (reqBody: CreateDashboardType) => {
   const res = await api.post('/dashboards', reqBody);
   return res.data;
 };
