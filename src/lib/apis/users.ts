7856dca2cb36c49babb0443d548215083845f1fe
aahreum <cocoding420@gmail.com>
2025-11-29 01:23:47 +0900

    ✨ Feat: 내 정보 수정 api 함수

--- src/lib/apis/users.ts
+++ src/lib/apis/users.ts
@@ -1,7 +1,18 @@
 import { api } from '@/lib/axios';
 
-/** 내 정보 조회 함수 */
+interface ChangeUserInfoType {
+  nickname: string;
+  profileImageUrl: string;
+}
+
+/** 내 정보 조회 api */
 export const getUsersMe = async () => {
   const res = await api.get('/users/me');
+  return res.data;
+};
+
+/** 내 정보 수정 api */
+export const changeUserMe = async (changeUserInfo: ChangeUserInfoType) => {
+  const res = await api.put('/users/me', changeUserInfo);
   return res.data;
 };
