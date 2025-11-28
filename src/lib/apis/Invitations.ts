49caa183311c27bb8ad0ac47d01dc4a03709f8be
aahreum <cocoding420@gmail.com>
2025-11-28 11:00:56 +0900

    🐛 Fix: 타입 string으로 변경

--- src/lib/apis/Invitations.ts
+++ src/lib/apis/Invitations.ts
@@ -1,7 +1,7 @@
 import { api } from '@/lib/axios';
 
 /** 대시보드 초대 함수 */
-export const inviteDashboard = async (dashboardId: number, reqBody: { email: string }) => {
+export const inviteDashboard = async (dashboardId: string, reqBody: { email: string }) => {
   const res = await api.post(`/dashboards/${dashboardId}/invitations`, reqBody);
   return res.data;
 };
