9fa72555ca0c4297e02e3b1fc51b482856de8f5b
aahreum <cocoding420@gmail.com>
2025-11-28 15:52:27 +0900

    🐛 Fix: className 추가

--- src/components/dashboard/dropdown/DropdownList.tsx
+++ src/components/dashboard/dropdown/DropdownList.tsx
@@ -1,9 +1,15 @@
 import useDropdownContext from '@/hooks/useDropdownContext';
+import { cn } from '@/utils/cn';
 
 // Dropdown 메뉴와 트리거 사이의 간격을 정의 (트리거 아래 8px)
 const DROPDOWN_MARGIN_TOP = 'top-[calc(100%+8px)]';
 
-export default function DropdownList({ children }: { children: React.ReactNode }) {
+interface DropdownListProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function DropdownList({ children, className }: DropdownListProps) {
   const { isOpen } = useDropdownContext();
 
   if (!isOpen) {
@@ -13,7 +19,11 @@ export default function DropdownList({ children }: { children: React.ReactNode }
   return (
     <ul
       role='menu'
-      className={`absolute right-0 z-1000 flex w-[93px] flex-col justify-center rounded-md bg-gray-0 px-1.5 py-1.75 shadow-[0_4px_20px_0_rgba(0,0,0,0.08)] ${DROPDOWN_MARGIN_TOP}`}>
+      className={cn(
+        'absolute right-0 z-1000 flex flex-col justify-center rounded-md bg-gray-0 px-1.5 py-1.75 shadow-[0_4px_20px_0_rgba(0,0,0,0.08)]',
+        ` ${DROPDOWN_MARGIN_TOP}`,
+        className
+      )}>
       {children}
     </ul>
   );
