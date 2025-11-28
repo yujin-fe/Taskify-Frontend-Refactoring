55762139ef544c88c79bd2a2bad6ba39afb1e700
aahreum <cocoding420@gmail.com>
2025-11-28 17:44:33 +0900

    ✨ Feat: 대시보드 스켈레톤 구현

---
+++ src/components/skeleton/DashboardSkeleton.tsx
@@ -0,0 +1,28 @@
+import Skeleton from '@/components/skeleton/Skeleton';
+
+export default function DashboardSkeleton({ length }: { length: number }) {
+  return (
+    <div className='flex'>
+      <aside className='fixed top-0 left-0 h-screen w-[300px] border-r border-gray-200 bg-gray-0 p-5'>
+        <Skeleton className='mb-[56px] h-[32px] w-[110px]' />
+        <Skeleton className='mb-3 h-4 w-20' />
+        <div className='flex flex-col gap-[8px]'>
+          {Array.from({ length: length }).map((_, i) => (
+            <Skeleton key={i} className='h-[42px] w-[260px]' />
+          ))}
+        </div>
+        <Skeleton className='mt-[32px] h-10 w-[80px]' />
+      </aside>
+      <div className='ml-[300px] w-full'>
+        <header className='fixed top-0 right-0 left-[300px] flex h-[70px] items-center border-b border-gray-200 bg-gray-0 px-[40px]'>
+          <Skeleton className='h-8 w-32' />
+          <div className='ml-auto flex gap-3'>
+            <Skeleton className='ml-6 h-10 w-10 rounded-full' />
+            <Skeleton className='h-[40px] w-[80px]' />
+          </div>
+        </header>
+        <main className='h-[100dvh] bg-base px-[40px] pt-[100px]' />
+      </div>
+    </div>
+  );
+}
