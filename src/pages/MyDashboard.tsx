03b92664d002d58ba83b380828b3e1425b916735
yujin-fe <hyujin1224@gmail.com>
2025-11-29 01:32:38 +0900

    🐛 Fix: 로딩, 에러 처리

--- src/pages/MyDashboard.tsx
+++ src/pages/MyDashboard.tsx
@@ -2,12 +2,14 @@ import PageIndicator from '@/components/common/PageIndicator';
 import PageNation from '@/components/common/PageNation';
 import CreateButton from '@/components/dashboard/CreateButton';
 import DashboardNameCard from '@/components/mydashboard/DashboardNameCard';
+import Skeleton from '@/components/skeleton/Skeleton';
 import { NEW_DASHBOARD } from '@/constants/modalName';
 import { useModal } from '@/hooks/useModal';
 import { usePagination } from '@/hooks/usePagination';
 import useQuery from '@/hooks/useQuery';
 import { getDashboards } from '@/lib/apis/dashboards';
 import { type DashboardsResponse, type GetDashboardsparams } from '@/types/dashboardsData';
+const PAGE_SIZE = 5;
 
 export default function MyDashboard() {
   const { currentPage, handlePrev, handleNext, isPrevDisabled } = usePagination();
@@ -16,20 +18,36 @@ export default function MyDashboard() {
   const params: GetDashboardsparams = {
     navigationMethod: 'pagination',
     page: currentPage,
-    size: 5,
+    size: PAGE_SIZE,
     cursorId: null,
   };
 
-  const { data: dashboardsData } = useQuery<DashboardsResponse>({
+  const {
+    data: dashboardsData,
+    isLoading,
+    error,
+  } = useQuery<DashboardsResponse>({
     fetchFn: () => getDashboards(params),
     params,
   });
+  if (isLoading) {
+    return (
+      <div className='flex flex-col gap-[70px] p-[24px]'>
+        <Skeleton className='h-[70px] w-[332px]' />
+        <Skeleton className='h-[390px] w-[960px]' />
+      </div>
+    );
+  }
+  //TODO: 에러컴포넌트로 교체
+  if (error) {
+    return <div>대쉬보드를 불러오는데 실패했습니다.</div>;
+  }
 
   if (!dashboardsData) {
     return null;
   }
 
-  const pageCount = Math.ceil(dashboardsData.totalCount / 5);
+  const pageCount = Math.ceil(dashboardsData.totalCount / PAGE_SIZE);
 
   return (
     <>
@@ -38,9 +56,13 @@ export default function MyDashboard() {
           <CreateButton className='h-[58px] font-lg-semibold' onClick={handleModalOpen}>
             새로운 대시보드
           </CreateButton>
-          {dashboardsData?.dashboards.map((dashboard) => (
-            <DashboardNameCard key={dashboard.id} dashboard={dashboard} />
-          ))}
+          {dashboardsData?.dashboards.map((dashboard, i) =>
+            isLoading ? (
+              <Skeleton key={`skeleton-${i}`} className='h-[70px] w-[332px]' />
+            ) : (
+              <DashboardNameCard key={dashboard.id} dashboard={dashboard} />
+            )
+          )}
         </div>
         <div className='flex items-center justify-end gap-4'>
           <PageIndicator currentPage={currentPage} totalPages={pageCount} />
