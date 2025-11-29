85323db7a00b337d1287e22f4b244e0ccaf9dd1f
aahreum <cocoding420@gmail.com>
2025-11-29 22:57:16 +0900

    ✨ Feat: 컬럼 조회 api 연결

--- src/pages/DashboardDetail.tsx
+++ src/pages/DashboardDetail.tsx
@@ -1,7 +1,11 @@
+import { useParams } from 'react-router';
 import CreateButton from '@/components/dashboard/CreateButton';
 import DashboardCard from '@/components/dashboard-detail/card/DashboardCard';
 import ColumnContainer from '@/components/dashboard-detail/column/ColumnContainer';
 import ColumnInfoHeader from '@/components/dashboard-detail/column/ColumnInfoHeader';
+import useQuery from '@/hooks/useQuery';
+import { getColumnList } from '@/lib/apis/columns';
+import type { ColumnsResponse } from '@/types/column';
 
 // TODO: 실제 데이터 연결 필요
 const cardDataArray = [
@@ -55,22 +59,37 @@ const cardDataArray = [
 ];
 
 export default function DashboardDetail() {
+  const { dashboardId } = useParams<{ dashboardId: string }>();
+
+  const { data: columnDataList, isLoading: isColumnLoading } = useQuery<ColumnsResponse>({
+    fetchFn: () => {
+      if (!dashboardId) {
+        throw new Error('dashboardId is missing');
+      }
+      return getColumnList(dashboardId);
+    },
+  });
+
   return (
     <div className='scrollbar-hidden flex flex-col overflow-hidden md:flex-row md:overflow-x-auto'>
-      <ColumnContainer>
-        {/* TODO: 실제 데이터 연결 및 함수 연결 */}
-        <ColumnInfoHeader
-          title='To do'
-          totalCount={5}
-          onClick={() => console.log('컬럼 수정 모달')}
-        />
-        <div className='flex flex-col gap-[16px]'>
-          <CreateButton onClick={() => console.log('할일 생성 모달')} />
-          {cardDataArray.map((card) => (
-            <DashboardCard key={card.id} cardData={card} />
-          ))}
-        </div>
-      </ColumnContainer>
+      {columnDataList?.data.map((column) => (
+        <ColumnContainer key={column.id}>
+          {/* TODO: totalCount는 카드 조회 모달에서 가져와야 함 */}
+          <ColumnInfoHeader
+            isLoading={isColumnLoading}
+            title={column.title}
+            totalCount={5}
+            onClick={() => console.log('컬럼 수정 모달')}
+          />
+          <div className='flex flex-col gap-[16px]'>
+            <CreateButton onClick={() => console.log('할일 생성 모달')} />
+            {/* TODO: 실제 데이터 연결 및 함수 연결 */}
+            {cardDataArray.map((card) => (
+              <DashboardCard key={card.id} cardData={card} />
+            ))}
+          </div>
+        </ColumnContainer>
+      ))}
     </div>
   );
 }
