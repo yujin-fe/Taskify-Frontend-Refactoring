4737bd5122a2af282157a8a437a222f44a5c6537
aahreum <cocoding420@gmail.com>
2025-11-30 00:47:14 +0900

    ♻️ Refactor: useQuery 에러 처리 수정

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -9,7 +9,7 @@ interface UseQueryType<TData, TParams = object> {
 const useQuery = <TData, TParams = object>({ fetchFn, params }: UseQueryType<TData, TParams>) => {
   const [data, setData] = useState<TData | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<unknown | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const fetchData = async () => {
     setIsLoading(true);
@@ -17,11 +17,14 @@ const useQuery = <TData, TParams = object>({ fetchFn, params }: UseQueryType<TDa
       const resData = await fetchFn(params);
       setData(resData);
     } catch (error: unknown) {
+      let errorMessage = '알 수 없는 오류가 발생했습니다.';
       if (axios.isAxiosError(error)) {
-        setError(error.response?.data);
-      } else {
-        setError(error);
+        errorMessage = error.response?.data?.message;
+      } else if (error instanceof Error) {
+        errorMessage = error.message;
       }
+      setError(errorMessage);
+      throw error;
     } finally {
       setIsLoading(false);
     }
