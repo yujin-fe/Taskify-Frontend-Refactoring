0922e262a665af74b7efef3f3336d0f23d978c1c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-29 14:39:09 +0900

    [✨ Feature/231] 마이페이지 - 프로필 수정 영역 구현 (#242)

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,11 +1,13 @@
 import BackButton from '@/components/dashboard/BackButton';
 import PasswordChangeForm from '@/components/mypage/PasswordChangeForm';
+import ProfileEditForm from '@/components/mypage/ProfileEditForm';
 
 export default function Mypage() {
   return (
     <div className='px-[12px] py-[16px] sm:p-[20px]'>
       <BackButton />
       <div className='mt-[10px] flex flex-col gap-[16px] sm:mt-[20px] sm:gap-[24px] md:mt-[34px]'>
+        <ProfileEditForm />
         <PasswordChangeForm />
       </div>
     </div>
