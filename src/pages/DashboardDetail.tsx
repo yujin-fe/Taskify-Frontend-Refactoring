1bfbf0ec7b589a58d2f1ab9aeb7a85d37644419b
aahreum <cocoding420@gmail.com>
2025-11-29 18:52:47 +0900

    🐛 Fix: height 제거

--- src/pages/DashboardDetail.tsx
+++ src/pages/DashboardDetail.tsx
@@ -1,10 +1,11 @@
 import CreateButton from '@/components/dashboard/CreateButton';
+import DashboardCard from '@/components/dashboard-detail/card/DashboardCard';
 import ColumnContainer from '@/components/dashboard-detail/column/ColumnContainer';
 import ColumnInfoHeader from '@/components/dashboard-detail/column/ColumnInfoHeader';
 
 export default function DashboardDetail() {
   return (
-    <div className='scrollbar-hidden flex h-[100dvh-70px] flex-col overflow-hidden md:flex-row md:overflow-x-auto'>
+    <div className='scrollbar-hidden flex flex-col overflow-hidden md:flex-row md:overflow-x-auto'>
       <ColumnContainer>
         {/* TODO: 실제 데이터 연결 및 함수 연결 */}
         <ColumnInfoHeader
@@ -14,17 +15,7 @@ export default function DashboardDetail() {
         />
         <div className='flex flex-col gap-[16px]'>
           <CreateButton onClick={() => console.log('할일 생성 모달')} />
-        </div>
-      </ColumnContainer>
-      <ColumnContainer>
-        {/* TODO: 실제 데이터 연결 및 함수 연결 */}
-        <ColumnInfoHeader
-          title='To do'
-          totalCount={5}
-          onClick={() => console.log('컬럼 수정 모달')}
-        />
-        <div className='flex flex-col gap-[16px]'>
-          <CreateButton onClick={() => console.log('할일 생성 모달')} />
+          <DashboardCard />
         </div>
       </ColumnContainer>
     </div>
