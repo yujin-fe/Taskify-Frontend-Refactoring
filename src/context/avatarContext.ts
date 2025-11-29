0aa83829505ec56f19ea1196bc661784ebd24da8
aahreum <cocoding420@gmail.com>
2025-11-29 21:18:40 +0900

    🐛 Fix: 아바타 context 타입 수정

--- src/context/avatarContext.ts
+++ src/context/avatarContext.ts
@@ -1,9 +1,11 @@
 import { createContext } from 'react';
+import type { Assignee } from '@/types/card';
+import type { Member } from '@/types/members';
 import type { UserMe } from '@/types/userMe';
 
-interface AvatarContextType extends UserMe {
+interface AvatarContextType {
   setImageError: React.Dispatch<React.SetStateAction<boolean>>;
-  userId?: number;
+  user: UserMe | Assignee | Member;
 }
 
 export const AvatarContext = createContext({} as AvatarContextType);
