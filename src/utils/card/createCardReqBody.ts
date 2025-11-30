71860fe1c877c3be29a70b6ed7c4fc587bd17519
aahreum <cocoding420@gmail.com>
2025-11-30 23:33:15 +0900

    ⚙️ Chore: 기본값 지정

--- src/utils/card/createCardReqBody.ts
+++ src/utils/card/createCardReqBody.ts
@@ -6,14 +6,26 @@ export const createCardRequestBody = (
   formValue: CardInitialValueType,
   columnId: number,
   dashboardId: string,
-  imageUrl: string | null
-): CreateCardType => ({
-  assigneeUserId: formValue.assigneeUser?.userId ?? 0,
-  dashboardId: Number(dashboardId),
-  columnId,
-  title: formValue.title,
-  description: formValue.description,
-  dueDate: formatDueDate(formValue.dueDate),
-  tags: formValue.tags,
-  imageUrl,
-});
+  imageUrl: string | null,
+  currentUserId?: number
+): CreateCardType => {
+  const formattedDueDate = formValue.dueDate
+    ? formatDueDate(formValue.dueDate)
+    : formatDueDate(new Date());
+
+  const normalizedTags = formValue.tags && formValue.tags.length > 0 ? formValue.tags : [''];
+  const assigneeUserId = formValue.assigneeUser?.userId ?? currentUserId ?? 0;
+
+  return {
+    assigneeUserId: assigneeUserId,
+    dashboardId: Number(dashboardId),
+    columnId,
+    title: formValue.title,
+    description: formValue.description,
+    dueDate: formattedDueDate,
+    tags: normalizedTags,
+    imageUrl:
+      imageUrl
+      ?? 'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/taskify/task_image/19-7',
+  };
+};
