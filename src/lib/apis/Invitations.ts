4115afa8e914fa103864dfb171dbebadbf921399
apple <apple@apples-MacBook-Air.local>
2025-11-30 22:18:11 +0900

    🐛 Fix: 배포 오류 수정

--- src/lib/apis/Invitations.ts
+++ src/lib/apis/Invitations.ts
@@ -1,9 +1,12 @@
 import { api } from '@/lib/axios';
-import { type InvitationsResponse } from '@/types/invitations';
-
+import type { InvitationParams, InvitationsResponse } from '@/types/invitations';
 /** 대시보드 초대 함수 */
 export const inviteDashboard = async (dashboardId: string, reqBody: { email: string }) => {
   const res = await api.post(`/dashboards/${dashboardId}/invitations`, reqBody);
+  return res.data;
+};
+export const getMyInvitations = async (params: InvitationParams) => {
+  const res = await api.get(`/invitations`, { params });
   return res.data;
 };
 
