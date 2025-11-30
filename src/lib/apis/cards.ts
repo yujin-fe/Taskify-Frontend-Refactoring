1dd4db55e35c70331f1d5204b3a2e63d8da98709
aahreum <cocoding420@gmail.com>
2025-11-30 21:42:19 +0900

    ✨ Feat: 카드 수정 api, 카드 생성 타입 수정

--- src/lib/apis/cards.ts
+++ src/lib/apis/cards.ts
@@ -1,14 +1,7 @@
 import { api, fileApi } from '@/lib/axios';
 
-export interface CreateCardType {
-  assigneeUserId: number | null;
+export interface CreateCardType extends ChangeCardType {
   dashboardId: number;
-  columnId: number;
-  title: string;
-  description: string;
-  dueDate: string | null;
-  tags: string[];
-  imageUrl: string | null;
 }
 
 interface GetCardDataParams {
@@ -17,7 +10,17 @@ interface GetCardDataParams {
   columnId: number;
 }
 
-/** 카드(할일) 생성 api */
+interface ChangeCardType {
+  columnId: number;
+  assigneeUserId: number;
+  title: string;
+  description: string;
+  dueDate: string | null;
+  tags: string[];
+  imageUrl: string | null;
+}
+
+/** 카드(할 일) 생성 api */
 export const createCard = async (reqBody: CreateCardType) => {
   const res = await api.post('/cards', reqBody);
   return res;
@@ -31,7 +34,7 @@ export const cardUploadImageFile = (columnId: number) => async (image: FormData)
   };
 };
 
-/** 카드 목록 조회 */
+/** 카드 목록 조회 api */
 export const getCardData = async (params: GetCardDataParams) => {
   const { size = 10, columnId, cursorId = null } = params;
   const res = await api.get('/cards', {
@@ -42,4 +45,10 @@ export const getCardData = async (params: GetCardDataParams) => {
     },
   });
   return res.data;
+};
+
+/** 카드(할 일) 수정 api */
+export const changeCard = async (cardId: number, reqBody: ChangeCardType) => {
+  const res = await api.put(`/cards/${cardId}`, reqBody);
+  return res;
 };
