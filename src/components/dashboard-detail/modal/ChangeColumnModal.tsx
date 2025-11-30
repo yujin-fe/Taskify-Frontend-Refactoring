5879063e62f3e2f1d96696ed7656e7ea04a42b0c
aahreum <cocoding420@gmail.com>
2025-11-30 14:12:56 +0900

    ♻️ Refactor: 부모에서 onDeleteModalOpen 제어하도록 변경

--- src/components/dashboard-detail/modal/ChangeColumnModal.tsx
+++ src/components/dashboard-detail/modal/ChangeColumnModal.tsx
@@ -2,21 +2,21 @@ import { useEffect, useState } from 'react';
 import Button from '@/components/common/Button';
 import Input from '@/components/common/input/Input';
 import FormModal from '@/components/common/modal/FormModal';
-import { CHANGE_COLUMN, DELETE_COLUMN } from '@/constants/modalName';
-import { useModal } from '@/hooks/useModal';
+import { CHANGE_COLUMN } from '@/constants/modalName';
 
 export interface ChangeColumnModalProps {
   initialName: string;
   onSubmit: (columnName: string) => Promise<void>;
   serverErrorMessage: string | null;
+  onDeleteModalOpen: () => void;
 }
 
 export default function ChangeColumnModal({
   initialName,
   onSubmit,
   serverErrorMessage,
+  onDeleteModalOpen,
 }: ChangeColumnModalProps) {
-  const { handleModalOpenOnly } = useModal(DELETE_COLUMN);
   const [columnName, setColumnName] = useState(initialName);
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -52,7 +52,7 @@ export default function ChangeColumnModal({
           </Input>
         </FormModal.Body>
         <FormModal.Footer className='pt-[24px]'>
-          <Button theme={'outlined'} onClick={handleModalOpenOnly}>
+          <Button theme={'outlined'} onClick={onDeleteModalOpen}>
             삭제
           </Button>
           <Button theme={'primary'} type='submit' disabled={disabled}>
