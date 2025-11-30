8c4629f7b9cc67d5233f864a358b8f60f8e93fb9
apple <apple@apples-MacBook-Air.local>
2025-11-30 14:54:16 +0900

    ✨ Feat: editpage 폴더 생성

--- src/pages/DashboardEdit.tsx
+++ src/pages/DashboardEdit.tsx
@@ -1,11 +1,11 @@
-import InvitsEdit from '@/pages/editpage/InviteEdit';
-import MembersEdit from '@/pages/editpage/MembersEdit';
+import InvitsEdit from '@/components/editpage/InviteEdit';
+import MembersEdit from '@/components/editpage/MembersEdit';
 
 export default function DashboardEdit() {
   return (
-    <>
+    <div className='ml-[12px] flex flex-col gap-4 sm:ml-[20px]'>
       <MembersEdit />
       <InvitsEdit />
-    </>
+    </div>
   );
 }
