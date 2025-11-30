627bafb9928ab8fa139068c10d6109902135f0f6
aahreum <cocoding420@gmail.com>
2025-11-30 12:40:13 +0900

    ✨ Feat: 오타 수정, delete column 키 추가

--- src/constants/modalName.ts
+++ src/constants/modalName.ts
@@ -1,4 +1,5 @@
 export const NEW_DASHBOARD = 'newDashboard';
 export const INVITE = 'inviteDashboard';
 export const CREATE_COLUMN = 'createColumn';
-export const CHANGE_COLUMN = 'chnageColumn';
+export const CHANGE_COLUMN = 'changeColumn';
+export const DELETE_COLUMN = 'deleteColumn';
