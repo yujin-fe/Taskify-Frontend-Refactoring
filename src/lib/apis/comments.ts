c484cb712ca0bc2673b0e8dbbd48f5f1b4adf783
aahreum <cocoding420@gmail.com>
2025-12-01 14:36:18 +0900

    ✨ Feat: 댓글 생성 api 추가

--- src/lib/apis/comments.ts
+++ src/lib/apis/comments.ts
@@ -7,10 +7,23 @@ interface GetCommentListParams {
   cardId: number;
 }
 
+interface CreateCommentType {
+  content: string;
+  cardId: number;
+  columnId: number;
+  dashboardId: number;
+}
+
 /** 댓글 조회 api */
 export const getCommentList = async (params: GetCommentListParams) => {
   const res = await api.get('/comments', {
     params,
   });
   return res.data;
+};
+
+/** 댓글 생성 api */
+export const createComment = async (reqBody: CreateCommentType) => {
+  const res = await api.post('/comments', reqBody);
+  return res;
 };
