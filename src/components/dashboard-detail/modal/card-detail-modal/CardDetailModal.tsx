3e7856d574e53df60015c4696f5a9623f2594224
aahreum <cocoding420@gmail.com>
2025-12-01 12:57:12 +0900

    ✨ Feat: 댓글 조회 api 연동

--- src/components/dashboard-detail/modal/card-detail-modal/CardDetailModal.tsx
+++ src/components/dashboard-detail/modal/card-detail-modal/CardDetailModal.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react';
 import ModalPortal from '@/components/common/modal/ModalPortal';
 import CardDetailModalDesktop from '@/components/dashboard-detail/modal/card-detail-modal/CardDetailModalDesktop';
 import CardDetailModalMobile from '@/components/dashboard-detail/modal/card-detail-modal/CardDetailModalMobile';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import { getCommentList } from '@/lib/apis/comments';
+import type { CommentListResponse } from '@/types/comment';
 
 export interface CardDetailModalContentProps {
   comment: string;
+  commentListData: CommentListResponse | null;
   setComment: React.Dispatch<React.SetStateAction<string>>;
   handleCommentSubmit: () => void;
   handleCardEdit: () => void;
@@ -13,11 +17,32 @@ export interface CardDetailModalContentProps {
 }
 
 interface CardDetailModal {
+  columnId: number;
+  cardId: number;
   closeModal: () => void;
 }
 
-export default function CardDetailModal({ closeModal }: CardDetailModal) {
+const COMMENT_LIST_SIZE = 5;
+
+export default function CardDetailModal({ closeModal, columnId, cardId }: CardDetailModal) {
   const [comment, setComment] = useState('');
+
+  const commentList = useInfiniteScroll<
+    CommentListResponse,
+    { size: number; cardId: number; columnId: number }
+  >({
+    fetchFn: (params) => getCommentList(params),
+    params: { size: COMMENT_LIST_SIZE, columnId, cardId },
+    onSuccess: (prev, next) => {
+      if (!prev) {
+        return next;
+      }
+      return {
+        ...next,
+        cards: [...prev.comments, ...next.comments],
+      };
+    },
+  });
 
   const handleCommentSubmit = () => {
     console.log('TODO: 댓글 등록');
@@ -35,6 +60,7 @@ export default function CardDetailModal({ closeModal }: CardDetailModal) {
     <ModalPortal>
       <div className='modal-dimmed'>
         <CardDetailModalDesktop
+          commentListData={commentList.data}
           comment={comment}
           setComment={setComment}
           handleCommentSubmit={handleCommentSubmit}
@@ -43,6 +69,7 @@ export default function CardDetailModal({ closeModal }: CardDetailModal) {
           handleCardDelete={handleCardDelete}
         />
         <CardDetailModalMobile
+          commentListData={commentList.data}
           comment={comment}
           setComment={setComment}
           handleCommentSubmit={handleCommentSubmit}
