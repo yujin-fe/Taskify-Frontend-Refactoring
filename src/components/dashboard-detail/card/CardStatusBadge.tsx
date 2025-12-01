d762fcc07227cd6a77f0da7427c16e4fff4a1750
aahreum <cocoding420@gmail.com>
2025-12-01 11:10:35 +0900

    🐛 Fix: shrink-0 추가

--- src/components/dashboard-detail/card/CardStatusBadge.tsx
+++ src/components/dashboard-detail/card/CardStatusBadge.tsx
@@ -9,7 +9,7 @@ export default function CardStatusBadge({ title, className }: CardStatusBadgePro
   return (
     <div
       className={cn(
-        'flex h-[26px] w-fit items-center gap-[6px] rounded-[16px] bg-violet-500-8 px-[10px] font-xs-regular select-none sm:h-[32px] sm:font-lg-regular',
+        'flex h-[26px] w-fit shrink-0 items-center gap-[6px] rounded-[16px] bg-violet-500-8 px-[10px] font-xs-regular select-none sm:h-[32px] sm:font-lg-regular',
         className
       )}>
       <div className='h-[6px] w-[6px] rounded-full bg-primary' />
