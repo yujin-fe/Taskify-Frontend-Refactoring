86af51c8e951d7cbf629061864faf750c6418e77
aahreum <cocoding420@gmail.com>
2025-12-01 20:48:30 +0900

    ✨ Feat: 카드 삭제 api 연결

--- src/lib/apis/cards.ts
+++ src/lib/apis/cards.ts
@@ -48,7 +48,7 @@ export const getCardListData = async (params: GetCardListDataParams) => {
 };
 
 /** 카드(할 일) 수정 api */
-export const changeCard = async (cardId: number, reqBody: ChangeCardType) => {
+export const updateCard = async (cardId: number, reqBody: ChangeCardType) => {
   const res = await api.put(`/cards/${cardId}`, reqBody);
   return res;
 };
@@ -57,4 +57,10 @@ export const changeCard = async (cardId: number, reqBody: ChangeCardType) => {
 export const getCardDetail = async (cardId: number) => {
   const res = await api.get(`/cards/${cardId}`);
   return res.data;
+};
+
+/** 카드 삭제 api */
+export const deleteCard = async (cardId: number) => {
+  const res = await api.delete(`/cards/${cardId}`);
+  return res;
 };
