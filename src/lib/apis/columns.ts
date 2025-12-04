dfa22ca8111c7841e230fdd86c938e2b77bcd6fb
aahreum <cocoding420@gmail.com>
2025-12-04 17:01:59 +0900

    🚚 Rename: change -> update로 변경

--- src/lib/apis/columns.ts
+++ src/lib/apis/columns.ts
@@ -6,12 +6,12 @@ export interface CreateColumnType {
   dashboardId: number;
 }
 
-export interface ChangeColumnVariables {
+export interface UpdateColumnVariables {
   columnId: number;
-  body: ChangeColumnType;
+  body: UpdateColumnType;
 }
 
-export interface ChangeColumnType {
+export interface UpdateColumnType {
   title: string;
 }
 
@@ -28,7 +28,7 @@ export const createColumn = async (reqBody: CreateColumnType) => {
 };
 
 /** 컬럼 수정 api */
-export const changeColumn = async (columnId: number, reqBody: ChangeColumnType) => {
+export const updateColumn = async (columnId: number, reqBody: UpdateColumnType) => {
   const res = await api.put(`/columns/${columnId}`, reqBody);
   return res;
 };
